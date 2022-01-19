9fe9069cbd46b08b0788b8a779a7d99875ba2a09
sinazarrin <goldmontain8@gmail.com>
2022-01-19 17:28:49 +0330

    change style not found page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './notFoundPage.css'
+import notFoundImage from './404.jpg'
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
         <>
             <div className="not-found">
-                <h2>404</h2>
-                <p>Page not found </p>
+                <h1>صحفه ی مورد نظر یافت نشد</h1>
+                <img src={notFoundImage} alt="" />
+                <Link to="/">
+                    بازگشت به صفحه ی اصلی
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-90deg-down" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M4.854 14.854a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V3.5A2.5 2.5 0 0 1 6.5 1h8a.5.5 0 0 1 0 1h-8A1.5 1.5 0 0 0 5 3.5v9.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4z" />
+                    </svg>
+                </Link>
             </div>
         </>
     );
